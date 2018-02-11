6574b2a5df325c35f9824650763406e125c8d34d
dingchaofa <571797089@qq.com>
2018-02-11 23:58:09 +0800

    index page

--- pages/hero-detail.vue
+++ pages/hero-detail.vue
@@ -1,3 +1,73 @@
 <template lang='pug'>
-  
+    #detail
+        .base-info
+            img(src='http://ossweb-img.qq.com/images/lol/web201310/skin/big266000.jpg')
+            div
+                h4 暗裔剑魔
+                h3 亚托克斯
+                span 战士
+                span 坦克
+                dl.hero-attr
+                    dt 物理攻击
+                    dd
+                        i(style="width:80%;background:#ffdc33;")
+                    dt 魔法攻击
+                    dd
+                        i(style="width:60%;background:#155ad8;")
+                    dt 防御能力
+                    dd
+                        i(style="width:40%;background:#da09ca;")
+                    dt 上手难度
+                    dd
+                        i(style="width:10%;background:#44bbb6;")
+        .bg-story
+            h3 背景故事
+            p 远古的暗裔族群中，曾有一位举世无双的剑术大师亚托克斯，他将浴血的战场看作狂欢的乐园。后来，他被敌人用魔法禁锢在了自己的剑中。他蛰伏了千年，只为等待一个合适的宿主到来—— 一位凡人武士持起亚托克斯的同时，也被这把活体武器侵蚀腐化，并改变了形态，至此，亚托克斯获得了重生。虽然这位暗裔的故事传说现已少有人信，但亚托克斯却始终铭记着自己种族的灭亡，一剑一剑地劈开自己的复仇之路。
+        .skill-info
+            h3 技能介绍
+            ul
+                li
+                    img(src='http://ossweb-img.qq.com/images/lol/img/passive/Aatrox_Passive.png')
+                    h6 1黑暗之跃 
+                        span 快捷键：Q
+                    p 亚托克斯纵身一跃然后猛击向目标位置，对落点中心的敌人造成20/50/80/110/140<span class="colorF88017">(+1.1)</span>物理伤害和持续1秒的击飞效果。<br>亚托克斯会在施放时使鲜血魔井的储量上升20%。
+                        
+                    dl
+                        dt 伤害：20/50/80/110/140
+                        dd 冷却时间：13/12.5/12/11.5/11
+                li
+                    img(src='http://ossweb-img.qq.com/images/lol/img/passive/Aatrox_Passive.png')
+                    h6 2黑暗之跃 
+                        span 快捷键：Q
+                    p 亚托克斯纵身一跃然后猛击向目标位置，对落点中心的敌人造成20/50/80/110/140<span class="colorF88017">(+1.1)</span>物理伤害和持续1秒的击飞效果。<br>亚托克斯会在施放时使鲜血魔井的储量上升20%。
+                        
+                    dl
+                        dt 伤害：20/50/80/110/140
+                        dd 冷却时间：13/12.5/12/11.5/11
+                li
+                    img(src='http://ossweb-img.qq.com/images/lol/img/passive/Aatrox_Passive.png')
+                    h6 3黑暗之跃 
+                        span 快捷键：Q
+                    p 亚托克斯纵身一跃然后猛击向目标位置，对落点中心的敌人造成20/50/80/110/140<span class="colorF88017">(+1.1)</span>物理伤害和持续1秒的击飞效果。<br>亚托克斯会在施放时使鲜血魔井的储量上升20%。
+                        
+                    dl
+                        dt 伤害：20/50/80/110/140
+                        dd 冷却时间：13/12.5/12/11.5/11
+        .recommend-equip
+            h3 推荐装备
+            h6 抱歉装备区域暂无法使用
+        .operation-skill
+            dl
+                dt 当你使用暗裔剑魔
+                dd
+                    p -【杀意已决】状态可在一场战斗中贡献显著的战斗力，因此在魔井较空时要避免接战。
+                    p - 出装方面，出一些生命偷取装和少量防御装可让亚托克斯变得更加能打，因为这么出会让他有更多时间来充分利用血涌效果。
+                dt 敌人使用暗裔剑魔
+                dd 
+                    p - 亚托克斯在【杀意已决】状态下会变得更加强大，因此在他能够浇灌鲜血魔井时，就要谨慎地与他作战。
+                    p - 亚托克斯的大部分伤害和生存能力都依赖于他的普通攻击，通过购买兰顿之兆或冰霜之心这类的减攻速装备可以帮助你极大的削减他的能力。
+
 </template>
+
+
+<style scoped lang='scss' src='../static/scss/detail.scss'></style>
